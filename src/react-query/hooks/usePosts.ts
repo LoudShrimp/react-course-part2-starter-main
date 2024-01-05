ccd5bb6d9c16c18193179798218f372794e92d56
LoudShrimp <PestLines@gmail.com>
2024-01-05 15:32:21 -0800

    more practice

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -8,12 +8,27 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => useQuery<Post[], Error>({
-  queryKey: ['posts'],
-  queryFn: () => axios
-    .get('https://jsonplaceholder.typicode.com/posts')
+interface PostQuery {
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => useInfiniteQuery<Post[], Error>({
+  // /users/1/posts
+  queryKey: ['posts', query],
+  queryFn: ({ pageParam = 1 }) => axios
+    .get('https://jsonplaceholder.typicode.com/posts', {
+      params: {
+        _start: (pageParam - 1) * query.pageSize,
+        _limit: query.pageSize
+      }
+    })
     .then((res) => res.data),
   staleTime: 1 * 60 * 1000, //1min
+  keepPreviousData: true,
+  getNextPageParam: (lastPage, allPages) => {
+    // 1 -> 2
+    return lastPage.length > 0 ? allPages.length + 1 : undefined;
+  }
 })
 
 export default usePosts;
